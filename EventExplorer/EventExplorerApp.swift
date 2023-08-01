dd843df4bf2cb7f06fd3a411448592b66362e091
Julien Pretet <j.pretet-ext@kreactive.com>
2023-08-01 08:52:12 +0200

    Initial Commit

---
+++ EventExplorer/EventExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  EventExplorerApp.swift
+//  EventExplorer
+//
+//  Created by Julien Pretet on 01/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct EventExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
